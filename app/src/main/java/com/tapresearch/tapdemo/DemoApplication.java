a65319b027eb5bea97b8a6d817c6f968aeb00132
Ilan Caspi <ilan.caspi@gmail.com>
2019-07-19 15:38:29 -0700

    v2.0.8

--- app/src/main/java/com/tapresearch/tapdemo/DemoApplication.java
+++ app/src/main/java/com/tapresearch/tapdemo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        TapResearch.configure("<API_TOKEN>", this);
+        TapResearch.configure("d425e0ebe93444912da2e121ae20c446", this);
         TapResearch.getInstance().setUniqueUserIdentifier("<USER_IDENTIFIER>");
     }
 }
